6b5d319025d9c3656af2d8ef03fd8d8a184c1786
DelgadoDevT <joaoteixeira9053@gmail.com>
2024-01-29 17:29:52 +0000

    Update to the initial menu of the executable

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,30 @@ import DataTypes ()
 
 import UnitConversion ()
 
-import Graphics.UI.Gtk ()
+import Graphics.UI.Gtk
 
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = do
+    _ <- initGUI
+    window1 <- windowNew
+    set window1 [windowTitle := "Haskonvert"
+               , windowDefaultWidth := 1400
+               , windowDefaultHeight := 1200
+               ]
+
+    fixed <- fixedNew
+    containerAdd window1 fixed
+
+    label1 <- labelNew (Nothing :: Maybe String)
+    set label1 [ labelLabel := "Haskonvert"
+              , labelUseMarkup := True
+              , widgetWidthRequest := 200
+              ]
+    font <- fontDescriptionNew
+    fontDescriptionSetFamily font "Serif"
+    fontDescriptionSetSize font 28
+    widgetModifyFont label1 (Just font)
+    fixedPut fixed label1 (510, 50)
+
+    widgetShowAll window1
+    mainGUI
