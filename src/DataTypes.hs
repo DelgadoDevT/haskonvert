874ee03682bb98f0a04b88b1054381306e2fc630
DelgadoDevT <joaoteixeira9053@gmail.com>
2024-01-29 13:29:41 +0000

    Added more data types

--- src/DataTypes.hs
+++ src/DataTypes.hs
@@ -3,7 +3,7 @@ Module: DataTypes
 Description: This module provides essential data types used across the program.
 -}
 
-module DataTypes where
+module DataTypes (module DataTypes) where
 
 data UnitsPack = LengthC LengthU
                | WeightC WeightU 
@@ -24,11 +24,11 @@ data TemperatureU = C | F | K deriving Show
 
 data SpeedU = Mps | Kmph | Miph | Cmps | Mips deriving Show
 
-data TimeU = S | Min | H | W | Mo | Y deriving Show
+data TimeU = S | Min | H | D | W | Mo | Y deriving Show
 
 data AreaU = Yd2 | Cm2 | Mi2 | Ft2 | Mm2 | Km2 | M2 deriving Show
 
-data VolumeU = Km3 | M3 | Dm3 | Mm3 | Ft3 | Yd3 | Gal | In3 deriving Show
+data VolumeU = Km3 | M3 | Dm3 | Mm3 | Ft3 | Yd3 | Gal | In3 | L deriving Show
 
 data EnergyU = J | Kj | Kwh | Cal | Kcal deriving Show
 
