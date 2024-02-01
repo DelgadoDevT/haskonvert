b3ef1530305e1ab438ec2e32a9543ae5094e5700
DelgadoDevT <joaoteixeira9053@gmail.com>
2024-02-01 13:39:12 +0000

    feat: home menu

--- app/Main.hs
+++ app/Main.hs
@@ -9,25 +9,74 @@ import Graphics.UI.Gtk
 main :: IO ()
 main = do
     _ <- initGUI
+
     window1 <- windowNew
-    set window1 [windowTitle := "Haskonvert"
-               , windowDefaultWidth := 1400
-               , windowDefaultHeight := 1200
-               ]
+    windowSetPosition window1 WinPosCenter
+    set window1 [ containerBorderWidth := 15
+                , windowTitle          := "Haskonvert"
+                , windowResizable      := False
+                , windowDefaultWidth   := 800
+                , windowDefaultHeight  := 800
+                ]
+    _ <- on window1 objectDestroy mainQuit
 
-    fixed <- fixedNew
-    containerAdd window1 fixed
+    table1 <- tableNew 4 3 True
+    tableSetColSpacing table1 0 10
+    tableSetColSpacing table1 1 10
+    tableSetRowSpacing table1 0 10
+    tableSetRowSpacing table1 1 10
+    tableSetRowSpacing table1 2 10
 
-    label1 <- labelNew (Nothing :: Maybe String)
-    set label1 [ labelLabel := "Haskonvert"
-              , labelUseMarkup := True
-              , widgetWidthRequest := 200
-              ]
-    font <- fontDescriptionNew
-    fontDescriptionSetFamily font "Serif"
-    fontDescriptionSetSize font 28
-    widgetModifyFont label1 (Just font)
-    fixedPut fixed label1 (510, 50)
+    image1 <- imageNewFromFile "src/Resources/logo.png"
+
+    button1 <- buttonNewWithLabel "Length"
+    _ <- on button1 buttonActivated $ do
+       putStrLn "Hello, World!"
+
+    button2 <- buttonNewWithLabel "Weigth"
+    _ <- on button2 buttonActivated $ do
+       putStrLn "Hello, World!"
+
+    button3 <- buttonNewWithLabel "Temperature"
+    _ <- on button3 buttonActivated $ do
+       putStrLn "Hello, World!"
+
+    button4 <- buttonNewWithLabel "Speed"
+    _ <- on button4 buttonActivated $ do
+       putStrLn "Hello, World!"
+
+    button5 <- buttonNewWithLabel "Time"
+    _ <- on button5 buttonActivated $ do
+       putStrLn "Hello, World!"
+
+    button6 <- buttonNewWithLabel "Area"
+    _ <- on button6 buttonActivated $ do
+       putStrLn "Hello, World!"
+
+    button7 <- buttonNewWithLabel "Volume"
+    _ <- on button7 buttonActivated $ do
+       putStrLn "Hello, World!"
+
+    button8 <- buttonNewWithLabel "Energy"
+    _ <- on button8 buttonActivated $ do
+       putStrLn "Hello, World!"
+
+    button9 <- buttonNewWithLabel "Byte"
+    _ <- on button9 buttonActivated $ do
+       putStrLn "Hello, World!"
+
+    tableAttachDefaults table1 image1  0 3 0 1
+    tableAttachDefaults table1 button1 0 1 1 2
+    tableAttachDefaults table1 button2 1 2 1 2
+    tableAttachDefaults table1 button3 2 3 1 2
+    tableAttachDefaults table1 button4 0 1 2 3
+    tableAttachDefaults table1 button5 1 2 2 3
+    tableAttachDefaults table1 button6 2 3 2 3
+    tableAttachDefaults table1 button7 0 1 3 4
+    tableAttachDefaults table1 button8 1 2 3 4
+    tableAttachDefaults table1 button9 2 3 3 4
+
+    containerAdd window1 table1
 
     widgetShowAll window1
     mainGUI
